9a314b0a16bc8dc7a817e4e902b86efdddc7af86
Muscle Everest <danza_seppia@yahoo.com>
2018-08-18 15:13:00 -0500

    final code

--- liri.js
+++ liri.js
@@ -9,6 +9,19 @@ var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 var command = process.argv[2];
 var searchItem = "";
+var dataLine1;
+var dataLine2;
+var dataLine3;
+var dataLine4;
+var dataLine5;
+var dataLine6;
+var dataLine7;
+var dataLine8;
+
+var commandLine = "";
+for (i = 0; i < process.argv.length; i++) {
+    commandLine += (process.argv[i] + " ");
+};
 
 for (i = 3; i < process.argv.length; i++) {
     searchItem += (process.argv[i] + " ");
@@ -34,8 +47,11 @@ switch (command) {
 function concertThis() {
     request("https://rest.bandsintown.com/artists/" + searchItem + "/events?app_id=codingbootcamp", function (error, response, body) {
         if (!error && response.statusCode === 200) {
-            console.log("\nThis band is playing at " + JSON.parse(body)[0].venue.name + ", " + JSON.parse(body)[0].venue.city + ", " + JSON.parse(body)[0].venue.region);
-            console.log(moment(JSON.parse(body)[0].datetime).format('MM/DD/YYYY'));
+            dataLine1 = "\nThis band is playing at " + JSON.parse(body)[0].venue.name + ", " + JSON.parse(body)[0].venue.city + ", " + JSON.parse(body)[0].venue.region;
+            dataLine2 = moment(JSON.parse(body)[0].datetime).format('MM/DD/YYYY');
+            console.log(dataLine1);
+            console.log(dataLine2);
+            logFile();
         }
     });
 };
@@ -48,32 +64,61 @@ function spotifyThis() {
         if (err) {
           return console.log('Error occurred: ' + err);
         }
-       
-      console.log("\nArtist: " + JSON.stringify(response.tracks.items[0].album.artists[0].name)); 
-      console.log("\nSong: " + JSON.stringify(response.tracks.items[0].name));
-      console.log("\nSpotify sample: " + JSON.stringify(response.tracks.items[0].album.artists[0].external_urls.spotify));
-      console.log("\nAlbum: " + JSON.stringify(response.tracks.items[0].album.name));
+       dataLine1 = "\nArtist: " + JSON.stringify(response.tracks.items[0].album.artists[0].name);
+       dataLine2 = "\nSong: " + JSON.stringify(response.tracks.items[0].name);
+       dataLine3 = "\nSpotify sample: " + JSON.stringify(response.tracks.items[0].album.artists[0].external_urls.spotify);
+       dataLine4 = "\nAlbum: " + JSON.stringify(response.tracks.items[0].album.name);
+      console.log(dataLine1); 
+      console.log(dataLine2);
+      console.log(dataLine3);
+      console.log(dataLine4);
+      logFile();
       });
 };
 
 function movieThis() {
     if (!searchItem) {
-        var searchItem = "mr nobody"
+        searchItem = "mr nobody"
     }
-    request("https://rest.bandsintown.com/artists/" + searchItem + "/events?app_id=codingbootcamp", function (error, response, body) {
+    request("http://www.omdbapi.com/?apikey=34d66101&t=" + searchItem, function (error, response, body) {
         if (!error && response.statusCode === 200) {
-            // console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+            dataLine1 = "Title: " + JSON.parse(body).Title;
+            dataLine2 = "Release Year: " + JSON.parse(body).Year;
+            dataLine3 = "IMDb Rating: " + JSON.parse(body).imdbRating;
+            dataLine4 = "Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value;
+            dataLine5 = "Country: " + JSON.parse(body).Country;
+            dataLine6 = "Language: " + JSON.parse(body).Language;
+            dataLine7 = "Plot: " + JSON.parse(body).Plot;
+            dataLine8 = "Actors: " + JSON.parse(body).Actors;
+            console.log(dataLine1);
+            console.log(dataLine2);
+            console.log(dataLine3);
+            console.log(dataLine4);
+            console.log(dataLine5);
+            console.log(dataLine6);
+            console.log(dataLine7);
+            console.log(dataLine8);
+            logFile();
         }
     });
 };
 
 function doWhat() {
-    var dataArr;
     fs.readFile("random.txt", "utf8", function(error, data) {
         if (error) {
             return console.log(error);
           }
-          dataArr = data.split(",");
+          var dataArr = data.split(",");
+          command = dataArr[0];
+          searchItem = dataArr[1];
+          spotifyThis();
     })
-    spotifyThis();
+};
+
+function logFile() {
+    fs.appendFile("log.txt", "\r\n" + commandLine + "\r\n" + dataLine1 + "\r\n" + dataLine2 + "\r\n" + dataLine3 + "\r\n" + dataLine4 + "\r\n" + dataLine5 + "\r\n" + dataLine6 + "\r\n" + dataLine7 + "\r\n" + dataLine8, function (error) {
+        if (error) {
+            return console.log(error);
+        }
+    });
 };
